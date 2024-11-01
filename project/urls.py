cf6e4a57e5e2476fca859b159b303e98e2f45dab
nonzoo <nonsoosadebe202@gmail.com>
2024-11-01 13:08:30 +0100

    test

--- project/urls.py
+++ project/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.views.generic.base import TemplateView
 from core.views import frontpage
 from django.conf.urls.i18n import i18n_patterns
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('i18n/',include('django.conf.urls.i18n')),
     path('admin/', admin.site.urls),
@@ -16,6 +16,7 @@ urlpatterns = [
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+urlpatterns += staticfiles_urlpatterns()
 '''urlpatterns = [
     
     path('admin/', admin.site.urls),]
